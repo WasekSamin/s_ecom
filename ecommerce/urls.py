e2db81cce3f56e66ca0c61e51b3c972761dc7dd4
Sakib <56402127+sakibovi123@users.noreply.github.com>
2021-04-07 17:16:47 +0600

    commit

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -14,16 +14,21 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 
 
 from django.views.static import serve
 from django.conf.urls import url
+from store import views
+from store.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
+    ## Path for Store App ##
+
+    path('', views.home, name="home"),
 
 
     url(r'^media/(?P<path>.*)$', serve,{'document_root':settings.MEDIA_ROOT}), 
