0e94532257d94277748f26f5357392f6f5b97573
Sakib <56402127+sakibovi123@users.noreply.github.com>
2021-04-08 14:34:57 +0600

    commit

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from store.models import *
 # Register your models here.
+
+
+admin.site.register(Brand)
+admin.site.register(SubCategory)
+admin.site.register(Category)
+admin.site.register(Shop)
+admin.site.register(Product)
--- store/models.py
+++ store/models.py
@@ -2,3 +2,56 @@ from django.db import models
 
 # Create your models here.
 
+
+class Brand(models.Model):
+    title = models.CharField(max_length=120)
+    img = models.ImageField(upload_to="images/")
+
+    def __str__(self):
+        return self.title
+
+class Category(models.Model):
+    title = models.CharField(max_length=120)
+    img = models.ImageField(upload_to="images/")
+
+
+    def __str__(self):
+        return self.title
+
+
+
+class SubCategory(models.Model):
+    title = models.CharField(max_length=120)
+    parent_category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, blank=True)
+
+
+    def __str__(self):
+        return self.title
+
+
+
+
+class Shop(models.Model):
+    title = models.CharField(max_length=120)
+    img = models.ImageField(upload_to="images/")
+
+    def __str__(self):
+        return self.title
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=120)
+    img = models.ImageField(upload_to="images/")
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE, null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, blank=True)
+    shop = models.ForeignKey(Shop, on_delete=models.CASCADE, null=True, blank=True)
+    isFavorite = models.BooleanField()
+    market_price = models.IntegerField()
+    sale_price = models.IntegerField()
+    stock_quantity = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True, blank=True)
+
+
+    def __str__(self):
+        return self.title
