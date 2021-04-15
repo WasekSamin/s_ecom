a2176dbab285ff89cad34ec09c13956fe1192038
Sakib <56402127+sakibovi123@users.noreply.github.com>
2021-04-16 02:23:21 +0600

    Debug True

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '1eh0k9buntzlx%s=@f=+ff+d(cgt0882=7-d%u28x^j-&nd!w$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'mustaqeem.herokuapp.com']
 
