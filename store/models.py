5560183f15c58aee28bbb26eede388bab36b121c
WasekSamin <54483009+WasekSamin@users.noreply.github.com>
2021-04-15 17:22:26 +0600

    Update models.py

--- store/models.py
+++ store/models.py
@@ -1,93 +1,110 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 # Create your models here.
 
 
+class Banner(models.Model):
+    image = models.ImageField(upload_to="images/", null=True)
+
+    def __str__(self):
+        return str(self.id)
+
+
+class LocalGroceryImage(models.Model):
+    image = models.ImageField(upload_to="images/", null=True)
+
+    def __str__(self):
+        return str(self.id)
+
+    def save(self):
+        super().save()
+        image = Image.open(self.image)
+        (width, height) = image.size
+        size = (800, 600)
+        image = image.resize(size, Image.ANTIALIAS)
+        image.save(self.image.path)
+
+
 class Brand(models.Model):
-    brand_slug = models.SlugField(unique=True, null=True, blank=True)
-    title = models.CharField(max_length=120, unique=True)
+    title = models.CharField(max_length=120, unique=True, null=True)
     img = models.ImageField(upload_to="images/")
 
     def __str__(self):
         return self.title
+
+    def save(self):
+        super().save()
+        image = Image.open(self.img)
+        (width, height) = image.size
+        size = (400, 400)
+        image = image.resize(size, Image.ANTIALIAS)
+        image.save(self.img.path)
+
 
 class Category(models.Model):
-    cat_slug = models.SlugField(unique=True, null=True, blank=True)
     title = models.CharField(max_length=120, unique=True)
     img = models.ImageField(upload_to="images/")
 
-
     def __str__(self):
         return self.title
-
 
 
 class SubCategory(models.Model):
-    sub_slug = models.SlugField(unique=True, null=True, blank=True)
     title = models.CharField(max_length=120, unique=True)
-    parent_category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, blank=True)
-
+    parent_category = models.ForeignKey(
+        Category, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.title
-
-
 
 
 class Shop(models.Model):
-    shop_slug = models.SlugField(unique=True, null=True, blank=True)
     title = models.CharField(max_length=120, unique=True)
     img = models.ImageField(upload_to="images/")
 
     def __str__(self):
         return self.title
 
-class Slider(models.Model):
-    slider_slug = models.SlugField(unique=True, null=True, blank=True)
-    title = models.CharField(max_length=120, unique=True)
+
+class Product(models.Model):
+    title = models.CharField(max_length=120)
     img = models.ImageField(upload_to="images/")
+    brand = models.ForeignKey(
+        Brand, on_delete=models.CASCADE, null=True, blank=True)
+    category = models.ForeignKey(
+        Category, on_delete=models.CASCADE, null=True)
+    shop = models.ForeignKey(
+        Shop, on_delete=models.CASCADE, null=True, blank=True)
+    isFavorite = models.BooleanField()
+    market_price = models.IntegerField()
+    sale_price = models.IntegerField(null=True, blank=True)
+    stock_quantity = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True, blank=True)
 
     def __str__(self):
         return self.title
+
 
 class Campaign(models.Model):
-    campaign_slug = models.SlugField(unique=True, null=True, blank=True)
+    campaign_slug = models.SlugField(unique=True, null=True)
     title = models.CharField(max_length=120, unique=True)
     img = models.ImageField(upload_to="images/")
-
 
     def __str__(self):
         return self.title
 
+
 class Offer(models.Model):
-    offer_slug = models.SlugField(unique=True, null=True, blank=True)
+    offer_slug = models.SlugField(unique=True, null=True)
     offer_title = models.CharField(max_length=120, unique=True)
     offer_img = models.ImageField(upload_to="images/")
-
 
     def __str__(self):
         return self.offer_title
 
-
-class Product(models.Model):
-    product_slug = models.SlugField(null=True, blank=True)
-    title = models.CharField(max_length=120)
-    img = models.ImageField(upload_to="images/")
-    brand = models.ForeignKey(Brand, on_delete=models.CASCADE, null=True, blank=True)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, blank=True)
-    shop = models.ForeignKey(Shop, on_delete=models.CASCADE, null=True, blank=True)
-    market_price = models.IntegerField()
-    sale_price = models.IntegerField()
-    stock_quantity = models.IntegerField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True, null=True, blank=True)
-    offer = models.ForeignKey(Offer, on_delete=models.CASCADE, null=True, blank=True)
-    campaign = models.ForeignKey(Campaign, on_delete=models.CASCADE, null=True, blank=True)
-
-
-    def __str__(self):
-        return self.title
 
 class WishList(models.Model):
     product = models.ForeignKey(Product, on_delete=models.DO_NOTHING)
